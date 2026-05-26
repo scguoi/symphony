9496d424af971144d5b77a0d88b15c56cf1ad8c3
scguoi <guosongchu@126.com>
2026-05-26 23:14:31 +0800

    test: cover plane tracker routing

--- elixir/test/symphony_elixir/extensions_test.exs
+++ elixir/test/symphony_elixir/extensions_test.exs
@@ -181,7 +181,7 @@ defmodule SymphonyElixir.ExtensionsTest do
     WorkflowStore.force_reload()
   end
 
-  test "tracker delegates to memory and linear adapters" do
+  test "tracker delegates to configured adapters" do
     issue = %Issue{id: "issue-1", identifier: "MT-1", state: "In Progress"}
     Application.put_env(:symphony_elixir, :memory_tracker_issues, [issue, %{id: "ignored"}])
     Application.put_env(:symphony_elixir, :memory_tracker_recipient, self())
@@ -203,6 +203,14 @@ defmodule SymphonyElixir.ExtensionsTest do
 
     write_workflow_file!(Workflow.workflow_file_path(), tracker_kind: "linear")
     assert SymphonyElixir.Tracker.adapter() == Adapter
+
+    write_workflow_file!(Workflow.workflow_file_path(),
+      tracker_kind: "plane",
+      tracker_workspace_slug: "forgeflow",
+      tracker_project_id: "project-uuid"
+    )
+
+    assert SymphonyElixir.Tracker.adapter() == Plane.Adapter
   end
 
   test "linear adapter delegates reads and validates mutation responses" do
